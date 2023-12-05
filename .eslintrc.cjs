dfdab4e88fc98ddbcc6391ca3c10e975d096c3e8
John Bellizzi <jabellizzi406@gmail.com>
2023-12-04 23:14:10 -0500

    linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: { node: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" },
+    ],
+    "prettier/prettier": "warn",
+  },
+};
--- who-picks-this-nose-client/.eslintrc.cjs
+++ who-picks-this-nose-client/.eslintrc.cjs
@@ -1,18 +1,11 @@
 module.exports = {
-  root: true,
+  extends: ["../.eslintrc.cjs", "plugin:react-hooks/recommended"],
+  plugins: ["react-refresh"],
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
-}
+};
