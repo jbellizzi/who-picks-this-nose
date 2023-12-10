0f5b797c911a010dcdb74eda0898aa41df1ff66c
John Bellizzi <jabellizzi406@gmail.com>
2023-12-10 17:25:02 -0500

    dumb

--- who-picks-this-nose-client/src/main.tsx
+++ who-picks-this-nose-client/src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
-import { SERVER_URI } from "./constants";
 import "./index.css";
 
 const client = new ApolloClient({
